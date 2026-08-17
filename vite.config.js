135e862b943d5b2d3714e7ac1363b9089cde09a6
akimdavy <153457047+akimdavy@users.noreply.github.com>
2026-08-17 20:09:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,26 @@
 import { defineConfig } from "vite";
 import { fileURLToPath } from "node:url";
 
-
 export default defineConfig({
+  base: "/ATA_aim-to-asset/",
 
   server: {
-
     cors: {
-      origin: "https://www.owlbear.rodeo"
-    }
-
+      origin: "https://www.owlbear.rodeo",
+    },
   },
 
-
   build: {
-
     rollupOptions: {
-
       input: {
-
         main: fileURLToPath(
-          new URL(
-            "./index.html",
-            import.meta.url
-          )
+          new URL("./index.html", import.meta.url)
         ),
 
         "shape-picker": fileURLToPath(
-          new URL(
-            "./shape-picker.html",
-            import.meta.url
-          )
-        )
-
-      }
-
-    }
-
-  }
-
+          new URL("./shape-picker.html", import.meta.url)
+        ),
+      },
+    },
+  },
 });
